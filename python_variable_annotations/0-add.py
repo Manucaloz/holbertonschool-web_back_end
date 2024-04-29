1373a164937b635b2731661c191726c2761663bf
nmanu7 <nelsoncalongos@gmail.com>
2024-04-28 19:56:24 -0500

    0. Basic annotations - add

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+''' Description: This code defines a function called add that takes two
+    arguments,a and b, which are both of type float. The function
+    returns the sum of a and b.
+    Arguments: a: float
+                b: float
+'''
+
+
+def add(a: float, b: float) -> float:
+    ''' Return sum of a and b. '''
+    return a + b
