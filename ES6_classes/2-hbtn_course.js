396733a3f43f9199223852f88e4c21927458971f
caloz7@hotmail.com <caloz7@hotmail.com>
2024-06-10 03:42:02 -0500

    ES6 classes

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -6,44 +6,44 @@ export default class HolbertonCourse {
       throw new TypeError('Name must be a string');
     }
     this._name = name;
-  
+
     if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
     }
     this._length = length;
-  
+
     if (!Array.isArray(students)) {
       throw new TypeError('Students must be a array');
     }
     this._students = students;
   }
-  
+
   get name() {
     return this._name;
   }
-  
+
   set name(name) {
     if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     }
     this._name = name;
   }
-  
+
   get length() {
     return this._length;
   }
-  
+
   set length(length) {
     if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
     }
     this._length = length;
   }
-  
+
   get students() {
     return this._students;
   }
-  
+
   set students(students) {
     if (!Array.isArray(students)) {
       throw new TypeError('Students must be a array');
@@ -51,4 +51,3 @@ export default class HolbertonCourse {
     this._students = students;
   }
 }
-  
--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,33 +1,33 @@
 // Class Pricing
 
 export default class Pricing {
-    constructor(amount, currency) {
-      this._amount = amount;
-      this._currency = currency;
-    }
-  
-    get amount() {
-      return this._amount;
-    }
-  
-    set amount(amount) {
-      this._amount = amount;
-    }
-  
-    get currency() {
-      return this._currency;
-    }
-  
-    set currency(currency) {
-      this._currency = currency;
-    }
-  
-    displayFullPrice() {
-      return `${this.amount} ${this.currency.displayFullCurrency()}`;
-    }
-  
-    static convertPrice(amount, conversionRate) {
-      return amount * conversionRate;
-    }
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
   }
+  
+  get amount() {
+    return this._amount;
+  }
+  
+  set amount(amount) {
+    this._amount = amount;
+  }
+  
+  get currency() {
+    return this._currency;
+  }
+  
+  set currency(currency) {
+    this._currency = currency;
+  }
+  
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
+  }
+  
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
   
