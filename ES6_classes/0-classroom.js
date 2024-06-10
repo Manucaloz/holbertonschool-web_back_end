2b6a594844e289416d430374b606ff2435753a4c
caloz7@hotmail.com <caloz7@hotmail.com>
2024-06-10 03:29:40 -0500

    ES6 classes

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,8 +1,7 @@
 // classRoom
 
 export default class ClassRoom {
-    constructor(maxStudentsSize) {
-      this._maxStudentsSize = maxStudentsSize;
-    }
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
   }
-  
+  }
