af731311fd3c778834faf8dc16858b709db50cbd
caloz7@hotmail.com <caloz7@hotmail.com>
2024-06-10 03:46:36 -0500

    ES6 classes

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -5,29 +5,28 @@ export default class Pricing {
     this._amount = amount;
     this._currency = currency;
   }
-  
+
   get amount() {
     return this._amount;
   }
-  
+
   set amount(amount) {
     this._amount = amount;
   }
-  
+
   get currency() {
     return this._currency;
   }
-  
+
   set currency(currency) {
     this._currency = currency;
   }
-  
+
   displayFullPrice() {
     return `${this.amount} ${this.currency.displayFullCurrency()}`;
   }
-  
+
   static convertPrice(amount, conversionRate) {
     return amount * conversionRate;
   }
 }
-  
